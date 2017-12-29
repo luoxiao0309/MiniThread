3b397b44cfdcfa6030df4f3305a56f0a8c068eac
liujun <liujun@moqikaka.com>
2017-12-29 15:51:51 +0800

    初步提交代码

---
+++ MiniThread/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniThread
+{
+    public class Context
+    {
+        public String GroupId { get; set; }
+
+        public void SetData(Object key, Object val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Object GetData(Object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AttachWaitObject()
+        {
+
+        }
+    }
+}
---
+++ MiniThread/MiniThreadSchedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MiniThread
+{
+    public class MiniThreadSchedule : IDisposable
+    {
+        public void Start(String GroupId, Func<Context, IEnumerator> funcObj)
+        {
+
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+
+        }
+    }
+
+    public class ThreadInfo
+    {
+        public Func<Context, IEnumerator> FuncObj { get; set; }
+
+        public IEnumerator Current { get; set; }
+
+        public ThreadInfo WaitingThread { get; set; }
+    }
+}
